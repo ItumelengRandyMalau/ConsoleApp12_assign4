a640a246e1fd12d3f89a6fe6450537bb3e4e1961
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2024-11-13 12:24:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp12_assign4
+{
+    internal class Program
+    {
+        class Person
+            //creating class fields
+        {
+            public string firstName;
+            public string lastName;
+            private int age;
+            public string gender;
+
+            //creating constructor to initialize the fields
+            public Person(string FirstName, string LastName, int Age, string Gender)
+            {
+                this.firstName = FirstName;
+                this.lastName = LastName;
+                this.age = Age;
+                this.gender = Gender;
+            }
+            public void introduce()
+            {
+                Console.WriteLine("Introducing...");
+                Console.WriteLine($"My name is {firstName}, {lastName}.");
+                Console.WriteLine($"I am {age} years old and the gender is {gender}.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            var person = new Person("Itumeleng Randy", "Malau", 26, "Female");
+            person.introduce();
+        }
+    }
+}
